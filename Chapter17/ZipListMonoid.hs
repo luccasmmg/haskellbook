74f7b6de3a1edb2f1fa835c21dcec9d4f2d2a0b8
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2021-07-05 21:14:04 -0300

    Magit problems

--- Chapter17/ZipListMonoid.hs
+++ Chapter17/ZipListMonoid.hs
@@ -4,3 +4,22 @@ module Chapter17.ZipListMonoid where
 
 import Control.Applicative
 import Data.Monoid
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
+instance Monoid a => Monoid (ZipList a) where
+  mempty = ZipList []
+  mappend = liftA2 mappend
+
+instance Semigroup a => Semigroup (ZipList a) where
+  (<>) = lift2A2 mappend
+
+instance Arbitrary a => Arbitrary (ZipList a) where
+  arbitrary = ZipList <$> arbitrary
+
+instance Arbitrary a => Arbitrary (Sum a) where
+  arbitrary = Sum <$> arbitrary
+
+instance Eq a => EqProp (ZipList a) where
+  (=-=) = eq
